ce8abc3b520147e8bac6c15f3e13b24e61d32caa
JakubSz-Pl <jszymanki08@gmail.com>
2026-03-03 13:54:11 +0100

    Aktualzacja danych na stronie
    API openweather zostalo poprawnie dołączone, możliwość wyswietlenia danych danego miasta defult(Warszawa)

--- script.js
+++ script.js
@@ -1,25 +1,96 @@
+const apiKey = "5bc794f045a5b618fe7551d6454f2c9e";
+let locationCity = "Warsaw";
 
-const apiKey = "";
-async function getWeather(city) {
-    
+
+
+
+
+async function getWeather() {
     try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=pl`);
-        let data = await response.json();
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${locationCity}&appid=${apiKey}&units=metric&lang=pl`
+        );
+        const data = await response.json();
         let daysData = [];
-        for (let i = 0; i < 40; i += 8) {
+        for (let i = 0; i < data.list.length; i += 8) {
             daysData.push(data.list[i]);
-            }
-
-        return daysData
+        }
+        console.log(daysData)
+        return daysData;
     } catch (error) {
-        console.error("error:", error);
+        console.error("Error:", error);
+        return [];
     }
 }
 
-let daysData = getWeather("Warsaw");
-console.log(daysData);
-const daysListTemp = document.getElementsByClassName("temp");
-console.log(daysListTemp);
-for (let i = 0; i < daysListTemp.length; i++) {
-  daysListTemp[i].innerHTML = daysData[i].main.temp;
+
+async function changeTemp() {
+
+    const daysData = await getWeather(locationCity);
+
+    const daysListTemp = document.getElementsByClassName("temp");
+
+    for (let i = 0; i < daysListTemp.length; i++) {
+        if (daysData[i]) {
+            daysListTemp[i].innerHTML = daysData[i].main.temp + "°C";
+        }
+    }
+}
+
+async function changePressure() {
+
+    const daysData = await getWeather(locationCity);
+
+    const daysListTemp = document.getElementsByClassName("pressure");
+
+    for (let i = 0; i < daysListTemp.length; i++) {
+        if (daysData[i]) {
+            daysListTemp[i].innerHTML = daysData[i].main.pressure + "hP";
+        }
+    }
+}
+async function changeFeelTemp() {
+
+    const daysData = await getWeather(locationCity);
+
+    const daysListTemp = document.getElementsByClassName("feelsLikeTemp");
+
+    for (let i = 0; i < daysListTemp.length; i++) {
+        if (daysData[i]) {
+            daysListTemp[i].innerHTML = daysData[i].main.feels_like + "°C";
+        }
+    }
+}
+async function changeWindSpeed() {
+
+    const daysData = await getWeather(locationCity);
+
+    const daysListTemp = document.getElementsByClassName("windSpeed");
+
+    for (let i = 0; i < daysListTemp.length; i++) {
+        if (daysData[i]) {
+            daysListTemp[i].innerHTML = daysData[i] .wind.speed + "km/h";
+        }
+    }
+}
+
+changeFeelTemp();
+changePressure();
+changeTemp();
+changeWindSpeed();
+
+
+
+
+function changeLocation() {
+    let input = document.querySelector(".locationInput");
+    let value = input.value;
+
+    document.getElementById("location").innerText = value;
+    locationCity = value;
+    
+changeFeelTemp();
+changePressure();
+changeTemp();
+changeWindSpeed();
 }
